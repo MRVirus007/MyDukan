0b0656bda634094f2725f45702161f607271f7ff
MRVirus007 <hackerpc77@gmail.com>
2020-11-12 12:06:20 +0530

    For Easy Practice

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,34 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: 'feed',
+        loadChildren: () =>
+          import('../pages/feed/feed.module').then(m => m.FeedPageModule)
+      },
+      {
+        path: 'messages',
+        loadChildren: () =>
+          import('../pages/messages/messages.module').then(
+            m => m.MessagesPageModule
+          )
+      },
+      {
+        path: 'notifications',
+        loadChildren: () =>
+          import('../pages/notifications/notifications.module').then(
+            m => m.NotificationsPageModule
+          )
+      },
+      {
+        path: 'settings',
+        loadChildren: () =>
+          import('../pages/settings/settings.module').then(
+            m => m.SettingsPageModule
+          )
+      }
+    ]
   }
 ];
 
@@ -13,4 +41,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
---
+++ src/app/pages/welcome/welcome.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { WelcomePageRoutingModule } from './welcome-routing.module';
+
+import { WelcomePage } from './welcome.page';
+import { ComponentsModule } from './../../components/components.module';
+import { RouterModule, Routes } from '@angular/router';
+import { SlidesComponent } from 'src/app/components/slides/slides.component';
+import { StartButtonComponent } from 'src/app/components/start-button/start-button.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ComponentsModule,
+
+    WelcomePageRoutingModule
+  ],
+  declarations: [WelcomePage]
+})
+export class WelcomePageModule { }
